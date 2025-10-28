b47db0b729f579ba803ec1977f234586fe983e07
Gowthami <gowthamikota25@gmail.com>
2025-10-28 18:33:45 +0530

    path correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import ForgotPassword from "./pages/Forgotpassword";
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
