70e7eb92fbe92a5f771170a7350a138f3d8f3577
Balu <balupasumarthi1@gmail.com>
2025-11-20 08:45:16 +0530

    add : main file

---
+++ python/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from analyzerprofile import analyze_profile
+from process import preprocess_user
+from resumeparser import parse_resume
+
+app = FastAPI()
+
+class UserIdRequest(BaseModel):
+    userId: str
+
+class FilePathRequest(BaseModel):
+    filePath: str
+
+
+@app.post("/preprocess")
+def preprocess(req: UserIdRequest):
+    return preprocess_user(req.userId)
+
+
+@app.post("/analyze-profile")
+def analyze(req: UserIdRequest):
+    return analyze_profile(req.userId)
+
+
+@app.post("/parse-resume")
+def resume(req: FilePathRequest):
+    return parse_resume(req.filePath)
